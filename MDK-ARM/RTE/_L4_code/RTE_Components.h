4068da9c451d7d9d837b45916a1e244d2d2e67aa
shiming422 <1611813195@qq.com>
2025-10-18 11:59:30 +0800

    init

---
+++ MDK-ARM/RTE/_L4_code/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'L4_code' 
+ * Target:  'L4_code' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
